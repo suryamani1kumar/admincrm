347ba73a710dc9dbba8e7a68b836b2481462a2ac
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-01-22 10:19:23 +0530

    Add text editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill";
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [value, setValue] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ReactQuill
+        value={value}
+        onChange={setValue}
+        formats={[
+          "header",
+          "font",
+          "size",
+          "bold",
+          "italic",
+          "underline",
+          "strike",
+          "blockquote",
+          "list",
+          "bullet",
+          "indent",
+          "link",
+          "image",
+          "video",
+          "color", // Include color formatting
+          "background", // Include background color formatting
+        ]}
+        modules={{
+          toolbar: [
+            [{ header: [1, 2, 3, 4, false] }],
+            [{ font: [] }],
+            [{ size: [] }],
+            ["bold", "italic", "underline", "strike", "blockquote"],
+            [{ color: [] }, { background: [] }], // Text and background color pickers
+            [
+              { list: "ordered" },
+              { list: "bullet" },
+              { indent: "-1" },
+              { indent: "+1" },
+            ],
+            ["link", "image", "video"],
+            ["clean"], // Remove formatting button
+          ],
+        }}
+      />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: value,
+        }}
+      />
     </>
   )
 }
