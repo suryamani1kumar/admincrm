e1fea71c5fc74b2c11cd22006e81344b857b7ea9
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-01-24 07:43:47 +0530

    login form

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,9 +1,22 @@
 import { HiOutlineUserCircle } from 'react-icons/hi';
 import { FaLock, FaUser } from 'react-icons/fa';
+import { BiSolidHide, BiSolidShow } from 'react-icons/bi';
+import { useState } from 'react';
 const Login = () => {
+  const [passwordHideShow, setPasswordHideShow] = useState(true);
+  const [loginDetails, setLoginDetails] = useState({
+    password: '',
+    userormail: '',
+  });
+  const handleloginDetails = (e) => {
+    const { value, name } = e.target;
+    setLoginDetails({
+      ...loginDetails,
+      [name]: value,
+    });
+  };
   return (
     <div className="loginconatiner">
-      {/* https://codepen.io/juniorthx3/pen/bGNxjEY */}
       <div className="login">
         <div className="login_form-box ">
           <div className="login_header-form">
@@ -19,7 +32,11 @@ const Login = () => {
               <input
                 type="text"
                 className="login_form-control"
-                placeholder="Username"
+                placeholder="Enter email or username"
+                name="userormail"
+                id="userormail"
+                value={loginDetails.userormail}
+                onChange={handleloginDetails}
               />
             </div>
             <div className="login_input-group">
@@ -29,17 +46,27 @@ const Login = () => {
                 </span>
               </div>
               <input
-                type="text"
+                type={passwordHideShow ? 'password' : 'text'}
+                placeholder="Enter Password"
                 className="login_form-control"
-                placeholder="Password"
+                name="password"
+                id="password"
+                value={loginDetails.password}
+                onChange={handleloginDetails}
               />
+              <span
+                className="passhidshow"
+                onClick={() => setPasswordHideShow(!passwordHideShow)}
+              >
+                {passwordHideShow ? <BiSolidHide /> : <BiSolidShow />}
+              </span>
             </div>
             <button type="button" className="btn btn-secondary btn-block">
               LOGIN
             </button>
             <div className="message">
-              <div>
-                <input type="checkbox" /> Remember ME
+              <div className="loginRemember">
+                <input type="checkbox" /> Remember me
               </div>
               <div>
                 <a href="#">Forgot your password</a>
