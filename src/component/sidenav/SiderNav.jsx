9da8a3fb99494bba6a892935ac78714d51bcb4cd
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-01-30 07:24:44 +0530

    remove div

--- src/component/sidenav/SiderNav.jsx
+++ src/component/sidenav/SiderNav.jsx
@@ -1,31 +1,15 @@
 import { useState } from 'react';
-import { BiExport } from "react-icons/bi";
-import { TbLogout, TbLogout2 } from "react-icons/tb";
-import { LuLayoutDashboard, LuLayoutGrid } from "react-icons/lu";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { FaRegUser } from "react-icons/fa";
-import { IoChevronDownSharp } from "react-icons/io5";
-import { IoMdClose, IoMdCloseCircle } from "react-icons/io";
+import { TbLogout } from 'react-icons/tb';
+import { LuLayoutDashboard } from 'react-icons/lu';
+import { FaRegUser } from 'react-icons/fa';
+import { IoMdClose } from 'react-icons/io';
 import {
-  MdArrowForwardIos,
-  MdKeyboardArrowRight,
   MdOutlineArrowForwardIos,
   MdOutlineKeyboardArrowDown,
-} from "react-icons/md";
+} from 'react-icons/md';
 
 const SiderNav = () => {
-  // const [isOpen, setIsOpen] = useState(true);
-  // const [activeSubMenu, setActiveSubMenu] = useState(null);
-
-  // const toggleSidebar = () => {
-  //   setIsOpen(!isOpen);
-  // };
-  // const handleMenuClick = (index) => {
-  //   setActiveSubMenu((prevActive) => (prevActive === index ? null : index));
-  // };
-  // const menuBtnClass = isOpen ? 'bx-menu-alt-right' : 'bx-menu';
-
-   const [activeMenu, setActiveMenu] = useState(null);
+  const [activeMenu, setActiveMenu] = useState(null);
   const [isSidebarActive, setIsSidebarActive] = useState(false);
 
   const handleMenuClick = (index) => {
@@ -39,67 +23,8 @@ const SiderNav = () => {
     <div>
       {/* https://codepen.io/neeraj_1/pen/mdgXdxb */}
       {/* https://codepen.io/kalvincalimag/pen/mdYbKoy */}
-{/*       <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-        <div className="logo-details">
-          <GiHamburgerMenu className="logo iconcontainersidebar" />
-          <div className="logo_name">Triploom</div>
-          <GiHamburgerMenu
-            onClick={toggleSidebar}
-            className={`siderbartoggle iconcontainersidebar ${menuBtnClass}`}
-          />
-        </div>
-        <ul className="nav-list">
-          <li>
-            <a href="#">
-              <LuLayoutGrid className="iconcontainersidebar" />
-              <span className="links_name">Dashboard</span>
-            </a>
-            <span className="tooltip">Dashboard</span>
-          </li>
-          <li>
-            <a href="#">
-              <FaRegUser className="iconcontainersidebar" />
-              <span className="links_name">User</span>
-              {/* <IoChevronDownSharp className="iconcontainersidebar" /> 
-               <ul className="sub-menu">
-                <li>
-                  <a href="#">
-                    <span className="text">Earnings</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <span className="text">Funds</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <span className="text">Declines</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <span className="text">Payouts</span>
-                  </a>
-                </li>
-              </ul> */}
-            </a>
 
-            <span className="tooltip">User</span>
-          </li>
-
-          <li className="profile">
-            <div className="profile-details">
-              <BiExport className="iconcontainersidebar" />
-              <div className="name_job">
-                <div className="name">Logout</div>
-              </div>
-            </div>
-            <TbLogout2 className="iconcontainersidebar" />
-          </li>
-        </ul>
-      </div> */}
-     <div className={`sidebar ${isSidebarActive ? "active" : ""}`}>
+      <div className={`sidebar ${isSidebarActive ? 'active' : ''}`}>
         <div className="menu-btn" onClick={toggleSidebar}>
           {isSidebarActive ? <MdOutlineArrowForwardIos /> : <IoMdClose />}
         </div>
@@ -122,7 +47,7 @@ const SiderNav = () => {
             <p className="title">Main</p>
             <ul>
               <li
-                className={activeMenu === 0 ? "active" : ""}
+                className={activeMenu === 0 ? 'active' : ''}
                 onClick={() => handleMenuClick(0)}
               >
                 <a href="#">
@@ -131,7 +56,7 @@ const SiderNav = () => {
                 </a>
               </li>
               <li
-                className={activeMenu === 1 ? "active" : ""}
+                className={activeMenu === 1 ? 'active' : ''}
                 onClick={() => handleMenuClick(1)}
               >
                 <a href="#">
@@ -142,7 +67,7 @@ const SiderNav = () => {
                   </span>
                 </a>
                 {activeMenu === 1 && (
-                  <ul className="sub-menu" style={{ display: "block" }}>
+                  <ul className="sub-menu" style={{ display: 'block' }}>
                     <li>
                       <a href="#">
                         <span className="text">Add Users</span>
