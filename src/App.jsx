6bb6c1e4cbcaaa41c7b4750810939f71c8001d43
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-02-17 18:28:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { routes } from './utils/routes';
 import Cookies from "js-cookie";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   useEffect(() => {
     const access_token = Cookies.get("a_token");
     const refesh_token = Cookies.get("r_token");
