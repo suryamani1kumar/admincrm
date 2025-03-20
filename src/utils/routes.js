254a96f7a15b766874c596081305cc4c6bd914e9
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-20 18:34:13 +0530

    Update routes.js

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,14 +1,16 @@
-import AddBlog from '../pages/addBlog';
-import Dashboard from '../pages/dashboard';
-import ForgetPassword from '../pages/ForgetPassword';
-import Login from '../pages/Login';
+import AddBlog from "../pages/addBlog";
+import Dashboard from "../pages/dashboard";
+import ForgetPassword from "../pages/ForgetPassword";
+import Login from "../pages/Login";
+import viewBlog from "../pages/viewBlog";
 
 export const protectedRoutes = [
-  { path: '/dashboard', Component: Dashboard },
-  { path: '/addblog', Component: AddBlog },
+  { path: "/dashboard", Component: Dashboard },
+  { path: "/addblog", Component: AddBlog },
+  { path: "/viewblog", Component: viewBlog },
 ];
 
 export const publicRoutes = [
-  { path: '/', Component: Login, needsProps: true },
-  { path: '/forgotpassword', Component: ForgetPassword },
+  { path: "/", Component: Login, needsProps: true },
+  { path: "/forgotpassword", Component: ForgetPassword },
 ];
