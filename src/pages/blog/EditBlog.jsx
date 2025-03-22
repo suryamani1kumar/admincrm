d5e0a79753f614caa841f722d590436e32597f87
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-03-22 10:55:44 +0530

    edit Blog modifiaction

--- src/pages/blog/EditBlog.jsx
+++ src/pages/blog/EditBlog.jsx
@@ -2,29 +2,45 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { axiosInstance } from '../../utils/axiosInstance';
 import TextEditor from '../../component/textEditor/TextEditor';
+import Loader from '../../component/loader/Loader';
 
 const EditBlog = () => {
   const { url } = useParams();
-  const [singleBlog, setSingleBlog] = useState(null);
+  const [singleBlog, setSingleBlog] = useState({});
+  const [laoding, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     axiosInstance
       .get(`/api/blog?pageurl=${url}`)
       .then((res) => {
         console.log('res', res);
-        setSingleBlog(res.data);
+        setSingleBlog(res.data.blog);
+        setLoading(false);
       })
       .catch((err) => console.log('error', err));
   }, []);
-  const updateBlog = () => {};
-  return (
-    singleBlog && (
-      <TextEditor
-        blogForm={singleBlog.blog}
-        setBlogFrom={setSingleBlog}
-        sumbit={updateBlog}
-      />
-    )
+
+  const updateBlog = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axiosInstance
+      .put(`/api/updateBlog?pageurl=${url}`, singleBlog)
+      .then((res) => {
+        console.log('res', res);
+        setLoading(false);
+      })
+      .catch((err) => console.log('error', err));
+  };
+
+  return laoding ? (
+    <Loader />
+  ) : (
+    <TextEditor
+      blogForm={singleBlog}
+      setBlogFrom={setSingleBlog}
+      sumbit={updateBlog}
+    />
   );
 };
 
